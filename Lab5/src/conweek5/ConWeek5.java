5231c69b1dbff9bbe07bd88c5dea0f5516cda905
12023081 <12023081@imail.sunway.edu.my>
2016-05-05 22:55:31 +0800

    Lab5

--- Lab5/src/conweek5/ConWeek5.java
+++ Lab5/src/conweek5/ConWeek5.java
@@ -20,11 +20,15 @@ public class ConWeek5 {
      */
     public static void main(String[] args) throws InterruptedException {
         double[][] a = new double[200][100];
-        Thread[] tArray = new Thread[200];
+        //Thread[] tArray = new Thread[200];
+        Thread[] tArray = new Thread[10];
         LinkedList minList = new LinkedList();
-
+        long start;
+        long end;
         Random rand = new Random();
         double r;
+
+        start = System.currentTimeMillis();
 
         for (int i = 0; i < 200; i++) {
             for (int j = 0; j < 100; j++) {
@@ -36,15 +40,28 @@ public class ConWeek5 {
             }
         }
 
-        for (int i = 0; i < 200; i++) {
-            EvaluateMinimum em = new EvaluateMinimum(a[i], minList);
-            tArray[i] = new Thread(em);
-            tArray[i].start();
+//        for (int i = 0; i < 200; i++) {
+//            EvaluateMinimum em = new EvaluateMinimum(a[i], minList);
+//            tArray[i] = new Thread(em);
+//            tArray[i].start();
+//        }
+//        for (int j = 0; j < 200; j++) {
+//            tArray[j].join();
+//        }
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                EvaluateMinimum em = new EvaluateMinimum(a[j], minList);
+                tArray[j] = new Thread(em);
+                tArray[j].start();
+
+            }
+            for (int j = 0; j < 10; j++) {
+                tArray[j].join();
+            }
         }
 
-        for (int i = 0; i < 200; i++) {
-            tArray[i].join();
-        }
+        end = System.currentTimeMillis();
+        System.out.println("Time needed to run program: " + (end - start) + "ms");
         System.out.printf("Minimum of all Threads: %.2f", (double) minList.get(minList.indexOf(Collections.min(minList))));
     }
 }
